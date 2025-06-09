2a7723899ad7352e3c368642b4318cf5f4a49575
harshitpahuja20 <harshit@loly.app>
2025-06-09 12:25:29 +0530

    code updated

--- app/cron/roundcreator.js
+++ app/cron/roundcreator.js
@@ -60,7 +60,7 @@ function logCombo(label, date) {
 }
 
 // CRON to close current round and create next round at :55 of every hour
-cron.schedule("24 * * * *", async () => {
+cron.schedule("55 * * * *", async () => {
   console.log(`\n[CRON 55] Triggered at ${new Date().toISOString()}`);
 
   try {
